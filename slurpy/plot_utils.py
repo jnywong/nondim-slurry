9e297c0602054e3cf70425aa31afb3358dfb2769
jnywong <jw53119@gmail.com>
2021-01-04 16:59:05 +0100

    plot seismic CD

--- slurpy/plot_utils.py
+++ slurpy/plot_utils.py
@@ -14,6 +14,7 @@ import pickle
 import os
 import matplotlib as mpl
 mpl.rcParams['text.usetex'] = False
+import matplotlib.gridspec as gridspec
 
 from slurpy.data_utils import readdata, get_outputDir
 from slurpy.getparameters import getcsbmassoxygen, getKphi, getphi
@@ -474,3 +475,141 @@ def plot_compare(layer_thickness,csb_heatflux,icb_heatflux,thermal_conductivity,
         plt.savefig('figures/profiles/'+saveName+'.pdf',format='pdf',dpi=200)
         
     plt.show()
+
+# %%
+def plot_CD(layer_thickness, thermal_conductivity,
+                             icb_heatflux, csb_heatflux,saveOn,figAspect=0.75):
+    w, h = plt.figaspect(figAspect)
+    # fig, (ax1,ax2,ax3) = plt.subplots(3,1,figsize=(1.25*w,1.25*h),sharex=True)
+    fig = plt.figure(constrained_layout=True,figsize=(1.25*w,2*h))
+    spec = gridspec.GridSpec(ncols=1, nrows=3, figure=fig)
+    ax1 = fig.add_subplot(spec[0, 0])
+    ax2 = fig.add_subplot(spec[1, 0],sharex=ax1)
+    ax3 = fig.add_subplot(spec[2, 0],sharex=ax1)
+
+    n = layer_thickness.size*thermal_conductivity.size \
+        *icb_heatflux.size*csb_heatflux.size
+
+    if n!=1:
+        fig_label = [r'high $Le$', r'low $Le$']
+        start = 0.2
+        stop = 0.5
+        cm_subsection = np.linspace(start, stop, n)
+        colors = [ cm.gray_r(x) for x in cm_subsection ]
+
+    # Load data
+    k=0
+    for w,x,y,z in [(w,x,y,z) for w in layer_thickness for x in icb_heatflux for y in csb_heatflux for z in thermal_conductivity]:
+        foldername, filename = get_outputDir(w,x,y,z)
+        inputDir = "results/{}/{}/".format(foldername,filename)
+
+        try:
+            inputs,outputs,profiles = readdata(inputDir)
+        except:
+            print('{} does not exist'.format(inputDir))
+            return
+
+        # Calculate bulk modulus from PREM
+        bulk_modulus = premvp(profiles.r)**2*premdensity(profiles.r)
+
+        # Calculate vp using slurry density and PREM bulk modulus
+        vp_slurry = np.sqrt(bulk_modulus/profiles.density)
+
+        # Calculate FVW P wave speed (Ohtaki et al. 2015, fig 11a)
+        x = profiles.r/earth_radius
+        vp_fvw = 3.3*x[0]-3.3*x +10.33
+
+        max_diff = np.max((vp_fvw-vp_slurry*1e-3)/vp_fvw*100)
+        print('Maximum difference with Ohtaki et al. (2015) is {:.2f}%'.format(max_diff))
+        max_diff = np.max((premvp(profiles.r)-vp_slurry)/premvp(profiles.r)*100)
+        print('Maximum difference with PREM is {:.2f}%'.format(max_diff))
+        print('Density on slurry side of ICB is {:.2f}'.format(profiles.density[0]))
+        density_jump = profiles.density[0] - profiles.density.iloc[-1]
+        print('Density jump is {:.2f}'.format(density_jump))
+        rho_bod = density_solidFe - profiles.density[0]
+        print('Delta rho bod is {:.2f}'.format(rho_bod))
+        rho_mod = rho_bod + density_jump
+        print('Delta rho mod is {:.2f}'.format(rho_mod))
+
+        # Plot P wave speed
+        if n==1:
+            ax3.plot(profiles.r*1e-3,vp_slurry*1e-3,color='darkgrey',lw=2,label='slurry density') #(km/s)
+            ax3.vlines(profiles.r[0]*1e-3,vp_slurry[0]*1e-3,10.4,color='darkgrey',lw=2)
+        else:
+            ax3.plot(profiles.r*1e-3,vp_slurry*1e-3,color=colors[k],lw=2,label=fig_label[k]) #(km/s)
+            ax3.vlines(profiles.r[0]*1e-3,vp_slurry[0]*1e-3,10.4,color=colors[k],lw=2)
+        # Check density
+        # ax3.plot(profiles.r*1e-3,premdensity(profiles.r),'k--')
+        # ax3.plot(profiles.r*1e-3,profiles.density)
+        k+=1
+    
+    # Plot temperature and composition
+    ax1.plot(profiles.r*1e-3,profiles.temp,lw=2,color='red',label='temperature')
+    csb_radius=profiles.r.iloc[-1]
+    radius_liquidus=np.linspace(icb_radius,csb_radius)
+    temp_liquidus=liquidus(radius_liquidus)
+    ax1.plot(radius_liquidus*1e-3,temp_liquidus,'k--', label = 'liquidus (Davies et al. 2015)')
+    (mass_conc_O,acore) = getcsbmassoxygen(inputs.oxygen_bulk)
+    acore=float(acore)
+    ax2.plot(profiles.r*1e-3,profiles.oxygen*acore/aO*100,lw=2,color='blue',label='oxygen')    
+    
+    # Plot FVW, PREM and AK135
+    ax3.plot(profiles.r*1e-3,vp_fvw,color='black',lw=2,ls=':',label='Ohtaki et al. (2015)')
+    ax3.vlines(profiles.r[0]*1e-3,vp_fvw[0],10.4,color='black',ls=':',lw=2)
+    ax3.plot(profiles.r*1e-3,premvp(profiles.r)*1e-3,color='k',ls='--',label='PREM')
+    ax3.vlines(profiles.r[0]*1e-3,premvp(profiles.r[0])*1e-3,10.4, 'k', linestyle='--')
+    # ax3.plot(radius_ak135*1e-3,vp_ak135*1e-3,'k',label='ak135')
+    # ax3.vlines(radius_ak135[0]*1e-3,vp_ak135[0]*1e-3,10.4, 'k')
+
+    ax1.legend(fontsize=11.5, loc='upper right')
+    ax2.legend(fontsize=11.5, loc='upper right')
+    ax3.legend(fontsize=11.5, loc='upper right')
+    ax1.set(ylabel="Temperature (K)")
+    ax2.set(ylabel="Oxygen (mol.%)")
+    ax3.set(xlabel="Radius (km)")
+    ax3.set(ylabel="P wave speed (km/s)")
+    ax3.set_xlim([1200,profiles.r.iloc[-1]*1e-3])
+    ax3.set_ylim([10.25,10.4])
+    major_xticks = np.arange(1200,1370,20)
+    minor_xticks = np.arange(1200,1370,5)    
+    ax1.set_xticks(major_xticks)
+    ax1.set_xticks(minor_xticks, minor=True)
+    ax2.set_xticks(major_xticks)
+    ax2.set_xticks(minor_xticks, minor=True)    
+    ax3.set_xticks(major_xticks)
+    ax3.set_xticks(minor_xticks, minor=True)
+    major_yticks = np.arange(5500,5751,100)
+    minor_yticks = np.arange(5500,5751,25)
+    ax1.set_yticks(major_yticks)
+    ax1.set_yticks(minor_yticks, minor=True)
+    ax1.grid(which='minor', alpha=0.2)
+    ax1.grid(which='major', alpha=0.5)
+    ax1.tick_params(which='major',length = 7)
+    ax1.tick_params(which='minor',length = 3.5)    
+    major_yticks = np.arange(6.5,8.1,0.5)
+    minor_yticks = np.arange(6.5,8.1,0.1)
+    ax2.set_yticks(major_yticks)
+    ax2.set_yticks(minor_yticks, minor=True)
+    ax2.grid(which='minor', alpha=0.2)
+    ax2.grid(which='major', alpha=0.5)
+    ax2.tick_params(which='major',length = 7)
+    ax2.tick_params(which='minor',length = 3.5)
+    major_yticks = np.arange(10.25,10.4,0.05)
+    minor_yticks = np.arange(10.25,10.4,0.01)
+    ax3.set_yticks(major_yticks)
+    ax3.set_yticks(minor_yticks, minor=True)
+    ax3.grid(which='minor', alpha=0.2)
+    ax3.grid(which='major', alpha=0.5)
+    ax3.tick_params(which='major',length = 7)
+    ax3.tick_params(which='minor',length = 3.5)
+
+    if saveOn==1:
+        saveDir='figures/seismic/'
+        if not os.path.exists(saveDir+foldername):
+            os.makedirs(saveDir+foldername)
+        fig.savefig(saveDir+foldername+"/"+filename+"_CD.pdf",format='pdf', dpi=200, bbox_inches='tight')
+        fig.savefig(saveDir+foldername+"/"+filename+"_CD.png",format='png', dpi=200, bbox_inches='tight')
+        print('Figure saved as {}'.format(saveDir+foldername+"/"+filename+"_CD.pdf"))
+    plt.show()
+
+    return profiles.r, vp_slurry, profiles.density, vp_fvw
