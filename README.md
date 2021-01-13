c6c8be72faf243959855498ba12bc37bfd0b1061
jnywong <>
2021-01-13 17:54:02 +0100

    simple unit test

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Python module to solve the 1D, steady, spherical slurry system outlined in Wong 
 ### Installing
 Conda:
 ```
-conda install nondim-slurry
+conda install -c jnywong nondim-slurry
 ```
 
 Pip:
---
+++ slurpy/tests/test_import.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+Simple test code for slurpy module
+"""
+
+import pytest
+from slurpy.coreproperties import icb_radius
+
+def output():
+    return icb_radius
+
+def test_output():
+    assert output() == 1221500.0
